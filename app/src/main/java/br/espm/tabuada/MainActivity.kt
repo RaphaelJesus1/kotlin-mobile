47e6baeff39069b0d8cbd6d808a3efbecd4c09f4
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2022-05-25 00:43:18 -0300

    final adjusts

--- app/src/main/java/br/espm/tabuada/MainActivity.kt
+++ app/src/main/java/br/espm/tabuada/MainActivity.kt
@@ -2,6 +2,8 @@ package br.espm.tabuada
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.Gravity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RatingBar
@@ -10,23 +12,34 @@ import androidx.appcompat.app.AlertDialog
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    var rightText: TextView = findViewById(R.id.rightNumber);
-    var leftText: TextView = findViewById(R.id.leftNumber);
-    var ratingBar: RatingBar = findViewById(R.id.ratingBar);
-    var input: EditText = findViewById(R.id.inputNumber);
-    var button: Button = findViewById(R.id.resultButton);
+    lateinit var rightText: TextView;
+    lateinit var leftText: TextView;
+    lateinit var ratingBar: RatingBar;
+    lateinit var input: EditText;
+    lateinit var button: Button;
 
     var expectedResult: Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rightText = findViewById(br.espm.tabuada.R.id.rightNumber);
+        rightText.textSize = 32F;
+        leftText = findViewById(br.espm.tabuada.R.id.leftNumber);
+        leftText.textSize = 32F;
+        ratingBar = findViewById(R.id.ratingBar);
+        ratingBar.setIsIndicator(true);
+        input = findViewById(R.id.inputNumber);
+        input.gravity = Gravity.CENTER_HORIZONTAL;
+        button = findViewById(R.id.resultButton);
+
+        generateNumbers();
         button.setOnClickListener() {
             checkResponse();
         }
     }
 
-    fun generateNumbers() {
+    private fun generateNumbers() {
         val num1 = Random.nextInt(1,9);
         val num2 = Random.nextInt(1,9);
         expectedResult = num1 * num2;
@@ -34,11 +47,14 @@ class MainActivity : AppCompatActivity() {
         leftText.text = num2.toString();
     }
 
-    fun checkResponse(){
+    private fun checkResponse(){
+        if(input.text.isEmpty()) {
+            return;
+        }
         // checar resultado
         val builder = AlertDialog.Builder(this);
         builder.setTitle("Resultado");
-        if(input.text.equals(expectedResult.toString())){
+        if(input.text.toString().toInt() == expectedResult){
             builder.setMessage("Acertou!!! Uhuul");
             //add estrela
             changeRating(1);
@@ -48,31 +64,30 @@ class MainActivity : AppCompatActivity() {
             changeRating(-1);
         }
         builder.setNeutralButton("Legal"){dialog, which -> dialog.cancel()};
-        builder.show();
 
         // dar resultado se alcancou 5 pontos
         if(ratingBar.rating >= 5) {
-            val builder = AlertDialog.Builder(this);
             builder.setTitle("PARABÉNS!!!");
             builder.setMessage("Você venceu o jogo.");
             builder.setNeutralButton("UHUL, I'M A WINNER"){dialog, which -> dialog.cancel(); resetGame()};
             builder.show();
             return;
         }
+        builder.show();
 
         // gerar numeros novamente
         generateNumbers();
         input.text.clear();
     }
 
-    fun changeRating(points: Int) {
+    private fun changeRating(points: Int) {
         if(points < 0 && ratingBar.rating == 0F) {
             return;
         }
         ratingBar.rating += points;
     }
 
-    fun resetGame() {
+    private fun resetGame() {
         input.text.clear();
         ratingBar.rating = 0F;
         generateNumbers();
